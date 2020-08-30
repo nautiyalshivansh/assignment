c6bf7aa66ea6ee59ab0ba064fba1fd49c06b28b0
nautiyalshivansh <69391250+nautiyalshivansh@users.noreply.github.com>
2020-08-30 20:55:14 +0530

    Delete q2.py

--- q2.py
+++
@@ -1,8 +0,0 @@
-i=int(input("enter lower limit"))
-j=int(input("input upper limit"))
-no=int(input("Enter the number"))
-if no in range(i,j+1):
-       print("Number in range ")
-else:
-    print("Number Not in range ")
-    
