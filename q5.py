6f84027ba52e92bfde5a375ee0ff42a3a94e5820
nautiyalshivansh <69391250+nautiyalshivansh@users.noreply.github.com>
2020-08-30 20:53:42 +0530

    Delete q5.py

--- q5.py
+++
@@ -1,9 +0,0 @@
-n=int(input("ENTER THE LIMIT OF LIST :"))
-list1=[]
-res=1
-print("ENTER THE NUMBERS :")
-for i in range(0,n):
-    a=int(input())
-    res*=a
-    list1.append(a)
-print("THE RESULT IS : ",res)
